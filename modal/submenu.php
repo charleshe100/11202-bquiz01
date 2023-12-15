5fe490bcc5e6840d89186519fcc2716cb395d749
charleshe100 <145732117+charleshe100@users.noreply.github.com>
2023-12-15 09:23:20 +0800

    submenu.php新增append和include db.php

--- modal/submenu.php
+++ modal/submenu.php
@@ -1,3 +1,4 @@
+<?php include_once "../api/db.php" ?>
 <h3>編輯次選單</h3>
 <hr>
 <form action="./api/submenu.php" method="post" enctype="multipart/form-data">
@@ -7,11 +8,19 @@
         <td>次選單連結網址:</td>
         <td>刪除</td>
     </tr>
+    <?php
+    $subs=$Menu->all(['menu_id'=>$_GET['id']]);
+    foreach($subs as $sub){    
+    ?>
     <tr>
-        <td><input type="text" name="text[]" id=""></td>
-        <td><input type="text" name="href[]" id=""></td>
-        <td><input type="checkbox" name="del" id=""></td>
+        <td><input type="text" name="text[]" value="<?=$sub['text'];?>"></td>
+        <td><input type="text" name="href[]" value="<?=$sub['href'];?>"></td>
+        <td><input type="checkbox" name="del" value="<?=$sub['id'];?>"></td>
+        <input type="hidden" name="id[]" value="<?=$sub['id'];?>">
     </tr>
+    <?php
+    }
+    ?>
     
 </table>
 <div>
@@ -22,3 +31,17 @@
 </div>
 
 </form>
+<script>
+
+function more(){
+    let item=`<tr>
+                <td><input type="text" name="text[]" id=""></td>
+                <td><input type="text" name="href[]" id=""></td>
+                <td><input type="checkbox" name="del" id=""></td>
+            </tr>`
+
+    $("#xxx").append(item);
+}
+
+
+</script>
